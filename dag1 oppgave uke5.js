9e12c853c98b56178742799cb451e7e52340ad01
sander1222 <sander122.kodehode@gmail.com>
2024-10-21 14:15:55 +0200

    Add files via upload

---
+++ dag1 oppgave uke5.js
@@ -0,0 +1,46 @@
+// en variabel er en beholder med et navn som kan refereres til og endres i koden, dette er et eksempel
+var x = 10;
+
+// du kan bruke en av tre måter som for eks
+var y = 2;
+const u = 3;
+let I = 4;
+
+// dette er de to mest brukte måtene å navngi variabler
+let this_Is_Camel_Case = "svar på oppgave 1";
+let this_is_snake_case = "svar på oppgave 1";
+
+// oppgave 2
+
+// dette er 5 forskjellige datatyper
+
+// Nummer, det er hele tall
+let alder = 21;
+
+// float er tall med desimaler
+let vekt = 87.6;
+
+// Boolean er en true or false
+let isWet = true;
+
+// array er en liste over verdier
+let mummer = [1, 3, 5, 7];
+
+//oppgave 3
+
+let x = 3;
+let y = 5;
+
+console.log(x + y);
+
+//oppgave 4
+// det er en variabel som bytter verdi i koden mer enn en gang
+
+let x = 3;
+let y = 4;
+
+console.log(x);
+
+let x = 7;
+
+console.log(x);
